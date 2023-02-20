30a9b8aa87f36e1896ab758f3d183237d908dce1
Ronald Muller <ronald.muller@silsistemas.com.br>
2023-02-20 12:58:26 -0300

    continuação

--- src/pages/success.tsx
+++ src/pages/success.tsx
@@ -1,5 +1,22 @@
+import Link from "next/link";
+import { ImageContainer } from "../styles/pages/product";
+import { SuccessContainer } from "../styles/pages/success";
+
 export default function Success() {
-    return (
-        <h1>Success</h1>
-    );
+	return (
+		<SuccessContainer>
+			<h1>Compra efetuada!</h1>
+			<ImageContainer>
+
+			</ImageContainer>
+
+			<p>
+               Uhuuu <strong>joaozinho</strong>, sua camiseta <strong>blablabla</strong> já está a caminho da sua casa. 
+			</p>
+
+            <Link href={"/"}>
+                Voltar ao catalogo
+            </Link>
+		</SuccessContainer>
+	);
 }
