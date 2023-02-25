32e22ae888eef994392a63ebabf877ae578773e5
Ronald Deived Muller <rdm.ronaldmuller@gmail.com>
2023-02-25 08:35:44 -0300

    finalizado

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -63,7 +63,7 @@ export const getStaticProps: GetStaticProps = async () => {
 			price: new Intl.NumberFormat("pt-BR", {
 				style: "currency",
 				currency: "BRL",
-			}).format(price.unit_amount! / 100),
+			}).format(price.unit_amount / 100),
 		};
 	});
 
